b9f07c21c6ac3bc445bbfefc8816b630366e034e
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-12 14:45:15 +0530

    Project Start

---
+++ unitrends/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <div className="text-white flex justify-center items-center h-screen w-screen">
+  HII PIYUSH AND TANISH HERE : )
+ </div>
+  );
+}

1bbf29bd754fcb6a15ca5ced540b9bd7c3ccf1df
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-13 00:39:01 +0530

    NAVBAR

---
+++ unitrends/src/app/mycomp/first.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+// import './App.css';
+
+// const First = () => {
+//     return (
+//         <div className="relative flex items-center justify-center h-screen">
+//           <a href="#" className="btn-shine">
+//             Get early access
+//           </a>
+//         </div>
+//       );
+// }
+
+// export default First

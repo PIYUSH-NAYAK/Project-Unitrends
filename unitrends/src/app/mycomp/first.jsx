144097387f076b049ef734ff1bea32baefeb0322
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-13 12:58:48 +0530

    first page

--- unitrends/src/app/mycomp/first.jsx
+++ unitrends/src/app/mycomp/first.jsx
@@ -1,14 +1,18 @@
-// import React from 'react'
-// import './App.css';
+import React from "react";
+import styles from "./styles.module.css"; 
 
-// const First = () => {
-//     return (
-//         <div className="relative flex items-center justify-center h-screen">
-//           <a href="#" className="btn-shine">
-//             Get early access
-//           </a>
-//         </div>
-//       );
-// }
+const First = () => {
+  return (
+    <div className="w-screen h-screen bg-black grid grid-cols-4">
+      <div className="col-span-2 h-5/6 bg-black z-10 flex flex-col justify-center items-center">
+        <a href="#" className={styles["btn-shine"]}>
+          <div className="text-6xl mb-5">WELCOME</div>
+          <div className="text-6xl mb-5 ">TO</div>
+          <div className="text-6xl mb-5">UNITRENDS</div>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-// export default First
+export default First;
--- unitrends/src/app/page.jsx
+++ unitrends/src/app/page.jsx
@@ -1,16 +1,17 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Navbar from "./mycomp/nav";
+import First from "./mycomp/first";
 
 export default function Home() {
   return (
     <div>
       <div className="text-white ">
-  <Navbar/>
-  {/* HII PIYUSH AND TANISH HERE : ) */}
-
- </div>
+        <Navbar />
+        {/* HII PIYUSH AND TANISH HERE : ) */}
+      </div>
+      <div>
+        <First/>
+      </div>
     </div>
-
- 
   );
 }

2a0a57172e070d46adbd4026327a80eeef4eb673
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-25 01:25:29 +0530

    adjust bubble size

--- unitrends/src/app/Section2/sec2.jsx
+++ unitrends/src/app/Section2/sec2.jsx
@@ -27,8 +27,8 @@ export default function Section2() {
       </nav>
       <div className="relative flex items-center justify-center w-full h-[calc(100vh-80px)] px-5 lg:px-20 overflow-hidden">
         {/* Animated background circles */}
-        <div className="absolute w-[300px] h-[300px] rounded-full left-[-6%] top-1/2 bg-gradient-to-r from-secondary to-pink-400 animate-object1 lg:w-[600px] lg:h-[600px]"></div>
-        <div className="absolute w-[100px] h-[100px] rounded-full left-[35%] top-[12%] bg-gradient-to-r from-secondary to-pink-400 animate-object2 lg:w-[200px] lg:h-[200px]"></div>
+        <div className="absolute w-[300px] h-[300px] rounded-full left-[6%] top-1/3 bg-gradient-to-r from-secondary to-pink-400 animate-object1 lg:w-[300px] lg:h-[300px]"></div>
+        <div className="absolute w-[100px] h-[100px] rounded-full left-[35%] top-[12%] bg-gradient-to-r from-secondary to-pink-400 animate-object2 lg:w-[100px] lg:h-[100px]"></div>
         
         {/* Text content */}
         <div className="z-10 w-full lg:w-1/2 px-4 lg:px-0">

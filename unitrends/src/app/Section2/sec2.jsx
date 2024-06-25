3260c6843a61641c4bc5fa6fb0c97adcda4fad00
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-06-25 11:07:45 +0530

    chnaged some animation

--- unitrends/src/app/Section2/sec2.jsx
+++ unitrends/src/app/Section2/sec2.jsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { useEffect } from 'react';
-import Typed from 'typed.js';
+import { useEffect } from "react";
+import Typed from "typed.js";
+import Image from "next/image";
 
 export default function Section2() {
   useEffect(() => {
@@ -21,31 +22,32 @@ export default function Section2() {
   }, []);
 
   return (
-    <div className="w-full min-h-screen bg-black overflow-hidden">
+    <div className="w-full min-h-min bg-black overflow-hidden">
       <nav className="flex justify-around h-20 items-center">
         {/* Navigation items */}
       </nav>
       <div className="relative flex items-center justify-center w-full h-[calc(100vh-80px)] px-5 lg:px-20 overflow-hidden">
         {/* Animated background circles */}
-        <div className="absolute w-[300px] h-[300px] rounded-full left-[-6%] top-1/2 bg-gradient-to-r from-secondary to-pink-400 animate-object1 lg:w-[600px] lg:h-[600px]"></div>
-        <div className="absolute w-[100px] h-[100px] rounded-full left-[35%] top-[12%] bg-gradient-to-r from-secondary to-pink-400 animate-object2 lg:w-[200px] lg:h-[200px]"></div>
-        
+        <div className="absolute w-[200px] h-[200px] rounded-full left-[2%] top-1/3 bg-gradient-to-r from-secondary to-pink-400 animate-object1 lg:w-[300px] lg:h-[300px]"></div>
+        <div className="absolute w-[100px] h-[100px] rounded-full left-[35%] top-[12%] bg-gradient-to-r from-secondary to-pink-400 animate-object2 lg:w-[150px] lg:h-[150px]"></div>
+
         {/* Text content */}
         <div className="z-10 w-full lg:w-1/2 px-4 lg:px-0">
           <h1 className="text-3xl lg:text-5xl font-extrabold text-white">
-            Show Your Spirit with Our College Gear: <span className="multiText text-typing_text"></span>
+            Show Your Spirit with Our College Gear:{" "}
+            <span className="multiText text-typing_text"></span>
           </h1>
         </div>
 
         {/* SVG section */}
         <div className="z-10 w-full lg:w-1/2 mt-10 lg:mt-0">
           <div className="relative h-full w-full flex items-center justify-center transform lg:rotate-y-180">
-            <img 
-              src="/section2.svg" 
-              alt="SVG Image" 
-              width={600} 
-              height={600} 
-              className="transform scale-[0.75] lg:scale-[1]" 
+            <Image
+              src="/section2.svg"
+              alt="SVG Image"
+              width={500}
+              height={500}
+              className="transform scale-[0.75] lg:scale-[1]"
             />
           </div>
         </div>
@@ -55,8 +57,8 @@ export default function Section2() {
       <style jsx>{`
         @keyframes object1 {
           50% {
-            left: -8%;
-            top: 45%;
+            left: 8%;
+            top: 24%;
           }
         }
         @keyframes object2 {

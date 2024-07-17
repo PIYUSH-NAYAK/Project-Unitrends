39607840058c34f74721b2b19bd82cd804ff6ba6
TanishMoral11 <ishantmoral@gmail.com>
2024-07-17 17:14:13 +0530

    Change Position

--- unitrends/src/app/page.jsx
+++ unitrends/src/app/page.jsx
@@ -4,11 +4,8 @@ import React, { useEffect, useState } from 'react';
 import Navbar from './mycomp/nav';
 import Section2 from './Section2/sec2';
 import AnimatedSection from './AnimatedSection/AnimatedSection';
-<<<<<<< HEAD
 import First from './mycomp/section3/first';
-=======
 import MotiveSection from './MotiveSection/motive'
->>>>>>> b9f50b6e5516eea45e1a3a03d96c0dbdaa39f820
 
 export default function Home() {
   const [animationData, setAnimationData] = useState(null);
@@ -19,11 +16,9 @@ export default function Home() {
       <Navbar />
       <Section2 />
       <AnimatedSection />
-<<<<<<< HEAD
-      <First/>
-=======
       <MotiveSection />
->>>>>>> b9f50b6e5516eea45e1a3a03d96c0dbdaa39f820
+      <First/>
+     
     </div>
   );
 }
